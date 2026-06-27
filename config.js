7447d11a26c2f20de496260eddb622476000b8c4
darioersantos <darioersantos@gmail.com>
2026-06-27 16:55:57 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+/* Endere√ßo da nuvem (Google Apps Script). Deixar vazio = guarda s√≥ neste dispositivo.
+   Depois de publicares o Apps Script, cola aqui o link /exec entre as aspas. */
+window.BACKEND_URL = "";
