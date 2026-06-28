204acf89b41aadfb2b3d8dcb0fa214f4494dcc31
darioersantos <darioersantos@gmail.com>
2026-06-28 10:30:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 /* Endereco da nuvem (Google Apps Script).
    Dados partilhados entre telemovel e computador.
    Para desligar a sincronizacao, deixar as aspas vazias. */
-window.BACKEND_URL = "https://script.google.com/macros/s/AKfycbxm-LysHJVpsbeVCNqI29WDHXIcFEwdYvhXn84LR6ASwoSuMDqh_PWJEI7danvz8lEr/exec";
+window.BACKEND_URL = "https://script.google.com/macros/s/AKfycbxGaFY8bv7lJcI2WbgoTqs_1mp9R85jw-CRkY-tTzsEPR_igqY-LMVtKSJ1hnsuiPlm/exec";
